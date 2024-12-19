058bd06bf19c5a2be54b6db08eebfc0131581c5d
bridgetf7 <fbridget0404@gmail.com>
2024-12-18 22:38:18 -0500

    mobile card styling

--- Barks-and-Beaks/src/components/itemCard.vue
+++ Barks-and-Beaks/src/components/itemCard.vue
@@ -1,45 +1,33 @@
 <template>
-  <div
-    class="card card-compact flex items-center justify-center p-3 shadow-lg h-auto w-full border border-base-400 border-l-base-300 border-t-base-200 border-r-base-300"
-    v-for="item in props.items"
-    :key="item"
-  >
-    <div class="avatar">
-      <div class="w-32 rounded-full overflow-hidden">
+    <div
+      class="du-card-side bg-base-100 shadow-xl flex m-3 h-64 rounded-md border border-[#859faf]/40"
+      v-for="item in props.items"
+      :key="item.id"
+    >
+      <figure class="">
         <img
           :src="item.image"
           :alt="item.name"
           class="h-full w-full object-cover"
         />
+      </figure>
+      <div class="du-card-body w-2/3 p-4">
+        <h2 class="du-card-title text-xl font-bold">{{ item.name }}</h2>
+        <p class="text-gray-600 line-clamp-2 truncate-clip">{{ item.description }}</p>
+        <div class="du-card-actions justify-end">
+          <button
+            @click="$router.push('/' + item.name)"
+            class="btn btn-primary w-full p-2 mt-3 font-bold bg-[#859faf] rounded-md"
+          >
+            <span class="flex items-center flex-row w-full h-full px-2">
+              <p class="text-black">Add to Cart</p>
+            </span>
+          </button>
+        </div>
       </div>
-    </div>
-    <div class="card card-body w-full">
-      <div class="card card-title">
-        <h1 class="text-2xl font-semibold text-center">
-          {{ item.name }}
-        </h1>
-        <p class="text-xl font-bold text-center">
-          ${{ parseFloat(item.options.price).toFixed(2) }}
-        </p>
-      </div>
-      <div class="card card-actions h-full justify-end w-full">
-        <button
-          @click="$router.push('/' + item.name)"
-          class="btn btn-primary w-full p-2 mt-3 font-bold"
-        >
-          <span class="flex items-center flex-row w-full h-full">
-            <p>Add to Cart</p>
-            <img
-              src="../assets/add-to-cart-svgrepo-com.svg"
-              alt="add to cart"
-              class="w-6 h-6"
-            />
-          </span>
-        </button>
-      </div>
-    </div>
   </div>
 </template>
+
 
 <script setup>
 const props = defineProps({
