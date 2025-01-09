bf231868f1dcba7cb356363e83977906a4455972
Benjamin Bernadskiy <bbernadskiy1224@gmail.com>
2025-01-09 14:01:12 -0500

    wow

--- Barks-and-Beaks/src/components/itemCard.vue
+++ Barks-and-Beaks/src/components/itemCard.vue
@@ -1,42 +1,44 @@
+<!-- ItemCard for Medium and large sized screens-->
+<!-- make a seperate card for phones or smaller screens-->
 <template>
   <div
-    class="card card-compact flex items-center justify-center p-3 shadow-lg h-auto w-full border border-base-400 border-l-base-300 border-t-base-200 border-r-base-300"
+    class="card rounded-[5px] card-compact flex items-center justify-center p-3 shadow-lg h-auto w-full border border-base-400 border-l-base-300 border-t-base-200 border-r-base-300 relative"
     v-for="item in props.items"
     :key="item"
   >
-    <div class="avatar">
-      <div class="w-32 rounded-full overflow-hidden">
-        <img
-          :src="item.image"
-          :alt="item.name"
-          class="h-full w-full object-cover"
-        />
+    <div class="border border-gray rounded-md h-40 w-11/12 overflow-hidden">
+      <img
+        :src="item.image"
+        :alt="item.name"
+        class="h-full w-full object-cover"
+      />
+    </div>
+    <div class="card card-body w-full flex flex-col">
+      <div class="flex flex-col justify-start">
+        <div class="card card-title items-start">
+          <h1 class="text-2xl font-semibold">
+            {{ item.name }}
+          </h1>
+          <p class="text-base">
+            {{ item.description }}
+          </p>
+          <p class="text-xl font-bold">
+            ${{ parseFloat(item.options.price).toFixed(2) }}
+          </p>
+        </div>
       </div>
     </div>
-    <div class="card card-body w-full">
-      <div class="card card-title">
-        <h1 class="text-2xl font-semibold text-center">
-          {{ item.name }}
-        </h1>
-        <p class="text-xl font-bold text-center">
-          ${{ parseFloat(item.options.price).toFixed(2) }}
-        </p>
-      </div>
-      <div class="card card-actions h-full justify-end w-full">
-        <button
-          @click="$router.push('/' + item.name)"
-          class="btn btn-primary w-full p-2 mt-3 font-bold"
-        >
-          <span class="flex items-center flex-row w-full h-full">
-            <p>Add to Cart</p>
-            <img
-              src="../assets/add-to-cart-svgrepo-com.svg"
-              alt="add to cart"
-              class="w-6 h-6"
-            />
-          </span>
-        </button>
-      </div>
+    <div class="">
+      <button
+        @click="$router.push('/' + item.name)"
+        class="btn rounded-full w-14 h-14 p-0 absolute bottom-[-14px] right-[-14px] flex items-center justify-center bg-gradient-to-t from-[#859FAF] to-[#8BC2E4]"
+      >
+        <img
+          src="../assets/add-to-cart-svgrepo-com.svg"
+          alt="add to cart"
+          class="w-6 h-6"
+        />
+      </button>
     </div>
   </div>
 </template>
