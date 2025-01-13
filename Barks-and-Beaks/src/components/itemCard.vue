f72f24cdc200a9193db45bde23f3d99f61f8a9a6
Benjamin Bernadskiy <bbernadskiy1224@gmail.com>
2025-01-13 14:00:07 -0500

    solved a few issues but its still like dum

--- Barks-and-Beaks/src/components/itemCard.vue
+++ Barks-and-Beaks/src/components/itemCard.vue
@@ -2,18 +2,18 @@
 <!-- make a seperate card for phones or smaller screens-->
 <template>
   <div
-    class="card rounded-[5px] card-compact flex items-center justify-center bg-[#D9D9D9] p-3 h-40 md:h-auto w-full relative"
+    class="card rounded-[5px] card-compact flex items-center justify-center bg-[#D9D9D9] p-3 h-32 md:h-40 w-full relative"
     v-for="item in props.items"
     :key="item"
   >
-    <div class="flex flex-row md:flex-col justify-between">
+    <div class="flex flex-row md:flex-col w-full justify-between">
       <div
-        class="border border-gray rounded-md h-30 md:h-40 w-1/2 md:w-11/12 overflow-hidden"
+        class="border border-gray rounded-md h-28 md:h-40 w-1/2 md:w-full overflow-hidden"
       >
         <img
           :src="item.image"
           :alt="item.name"
-          class="h-30 md:h-full w-full md:object-cover"
+          class="h-full w-full object-cover"
         />
       </div>
       <div class="card card-body w-1/2 md:w-full flex flex-col">
@@ -22,7 +22,7 @@
             <p class="text-base font-bold text-black">
               {{ item.name }}
             </p>
-            <p class="text-base text-black font-semibold hidden md:visible">
+            <p class="text-base text-black font-semibold hidden md:block">
               {{ item.description }}
             </p>
             <p class="text-base font-bold text-black">
@@ -37,7 +37,8 @@
     >
       <button
         @click="$router.push('/' + item.name)"
-        class="btn rounded-full w-14 h-14 flex bg-gradient-to-t bg-[#86A4B6] md:from-[#859FAF] md:to-[#8BC2E4]"
+        class="btn rounded-full w-14 h-14 flex bg-gradient-to-t from-[#86A4B6] to-[#8BC2E4]"
+        type="button"
       >
         <img
           src="../assets/add-to-cart-svgrepo-com.svg"
